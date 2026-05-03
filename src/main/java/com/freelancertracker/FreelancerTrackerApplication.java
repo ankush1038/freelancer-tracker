70a6b8fa0cc0f121422ff8e2d289fe484b18e2bd
Ankush Sharma <ankusharma1029@gmail.com>
2026-05-03 17:42:59 +0530

    DB connection done

---
+++ src/main/java/com/freelancertracker/FreelancerTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.freelancertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelancertracker/FreelancerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelancertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
